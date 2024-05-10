1193cae9a443d1a4b8069de3ba84ec6b8fece0a5
flour <99916123+Flour510@users.noreply.github.com>
2024-05-10 15:13:33 -0700

    Phase 1 and Phase 2ish

--- FlowerPowerMotors/src/main/java/com/pluralsight/DealershipFileManager.java
+++ FlowerPowerMotors/src/main/java/com/pluralsight/DealershipFileManager.java
@@ -1,4 +1,36 @@
 package com.pluralsight;
 
-public class DealershipFileManager {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class DealershipFileManager
+{
+    public Dealership getDealership()
+    {
+        // file path
+        String filePath = "file/inventory.csv";
+
+        // file object that points to the text file
+        File file = new File(filePath);
+
+        // create a file reader to open the file (Scanner)
+        try(Scanner fileScanner = new Scanner(file))
+        {
+            // read the contents of the file
+            while(fileScanner.hasNext())
+            {
+
+            }
+        }
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("File not found. ");
+        }
+    }
+
+    public Dealership saveDealership()
+    {
+        return null;
+    }
 }
