358a6af6d1fa9a40a53fb05ea7c380419a880a61
flour <99916123+Flour510@users.noreply.github.com>
2024-05-13 08:12:53 -0700

    Tiny Update

---
+++ FlowerPowerMotors/src/test/java/com/pluralsight/Test.java
@@ -0,0 +1,17 @@
+//package com.pluralsight;
+//
+//public class Test {
+//
+//    @Test
+//    public void vehicle_is_correct()
+//    {
+//        // arrange - setup
+//        Vehicle vehicle = new Vehicle(int id, int year, String make, String model, String color, double price);
+//
+//        // act --> THE ONE action that I am testing
+//
+//        // assert -  verify that the action worked as expected
+//
+//    }
+//
+//}
