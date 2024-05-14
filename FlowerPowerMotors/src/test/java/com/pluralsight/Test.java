c39cd1dd1e246951c24909def389eeaf4a4df607
flour <99916123+Flour510@users.noreply.github.com>
2024-05-13 17:03:52 -0700

    Test Updated

--- FlowerPowerMotors/src/test/java/com/pluralsight/Test.java
+++ FlowerPowerMotors/src/test/java/com/pluralsight/Test.java
@@ -1,17 +1,49 @@
-//package com.pluralsight;
-//
-//public class Test {
-//
-//    @Test
-//    public void vehicle_is_correct()
-//    {
-//        // arrange - setup
-//        Vehicle vehicle = new Vehicle(int id, int year, String make, String model, String color, double price);
-//
-//        // act --> THE ONE action that I am testing
-//
-//        // assert -  verify that the action worked as expected
-//
-//    }
-//
-//}
+package com.pluralsight;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class DealershipTest
+{
+    private Dealership dealership;
+
+    @BeforeEach
+    public void setUp()
+    {
+        // sample dealership to test
+        dealership = new Dealership("Test Dealership", "Oakland Ca", "510-444-4444");
+        dealership.addVehicle(new Vehicle(0, 4000, "Volkswagen", "Flying Beetle", "Opal", 900000));
+        dealership.addVehicle(new Vehicle(1, 50, "Donkey", "Mammal", "Grey", 20));
+    }
+
+    @Test
+    public void testListAll_Vehicles()
+    {
+        // arrange
+        List<Vehicle> vehicleList = dealership.getAllVehicles();
+
+        // act
+        assertEquals(2, vehicleList.size());
+
+        // assert
+        Vehicle firstVehicle = vehicleList.get(0);
+        assertEquals(0, firstVehicle.getId());
+        assertEquals(4000, firstVehicle.getYear());
+        assertEquals("Volkswagen", firstVehicle.getMake());
+        assertEquals("Flying Beetle", firstVehicle.getModel());
+        assertEquals("Opal", firstVehicle.getColor());
+        assertEquals(900000, firstVehicle.getPrice());
+
+        Vehicle secondVehicle = vehicleList.get(1);
+        assertEquals(1, secondVehicle.getId());
+        assertEquals(50, secondVehicle.getYear());
+        assertEquals("Donkey", secondVehicle.getMake());
+        assertEquals("Mammal", secondVehicle.getModel());
+        assertEquals("Grey", secondVehicle.getColor());
+        assertEquals(20, secondVehicle.getPrice());
+    }
+}
